e48e2fbc37fcfa02dd3eacac75f956baaf79475c
Bhavya <bhavyahshree@gmail.com>
2026-04-29 12:14:18 -0700

    Update Experience.jsx

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,39 +1,38 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import './index.css';
 
 function Experience() {
   const experiences = [
     {
-      year: 'June 2025 - Current', 
-      title: 'Undergraduate Researcher', 
-      org: 'UW Department of Genome Sciences', 
+      year: 'June 2025 - Current',
+      title: 'Undergraduate Researcher',
+      org: 'UW Department of Genome Sciences',
       projects: [
-        'Training Casanovo (de novo peptide sequencer with timsTOF data)', 
+        'Training Casanovo (de novo peptide sequencer with timsTOF data)',
         'Integrating DIA functionality into Casanovo',
       ],
       skills: [
-        'Research documentation + Deep learning', 'Mass spectrometry', 
-      ]
+        'Research documentation + Deep learning',
+        'Mass spectrometry',
+      ],
     },
     {
       year: 'May 2024 - January 2026',
       title: 'Assistant Researcher',
       org: 'UW Department of Bioengineering',
       projects: [
-        'Created RAG chatbot using Biomodels to aid biological researchers', 
-        'Created a comprehensive pipeline to analyze research papers, made large repositories of information used to fine-tune LLMs in the lab', 
-        'Implemented AI models using scikit-learn, , PyTorch, Keras to predict future concentrations of species', 
-        'Created AI models to recover biological networks from time series data'
+        'Created RAG chatbot using Biomodels to aid biological researchers',
+        'Created a comprehensive pipeline to analyze research papers, made large repositories of information used to fine-tune LLMs in the lab',
+        'Implemented AI models using scikit-learn, PyTorch, Keras to predict future concentrations of species',
+        'Created AI models to recover biological networks from time series data',
       ],
-      skills: [ 
-        'PyTorch + Tensorflow + Keras + Scikit-Learn', 'JavaScript', 'Streamlit'
+      skills: [
+        'PyTorch + TensorFlow + Keras + Scikit-Learn',
+        'JavaScript',
+        'Streamlit',
       ],
       publications: [
-        'https://arxiv.org/abs/2601.22684,
-      ]
+        'https://arxiv.org/abs/2601.22684',
+      ],
     },
     {
       year: 'September 2024 - August 2025',
@@ -41,10 +40,13 @@ function Experience() {
       org: 'UW iGEM',
       projects: [
         'Developed Model Predictive Control (MPC) algorithms to model large scale bioreactors',
-        'Cleaned and collected data using Microsoft Power Automate'
+        'Cleaned and collected data using Microsoft Power Automate',
       ],
       skills: [
-        'Microsoft Suite', 'do-mpc (Python)', 'Differential Equations', 'Data Visualization/Analaysis/Management'
+        'Microsoft Suite',
+        'do-mpc (Python)',
+        'Differential Equations',
+        'Data Visualization/Analysis/Management',
       ],
     },
     {
@@ -52,12 +54,15 @@ function Experience() {
       title: 'Research Assistant',
       org: 'Harvard Medical School',
       projects: [
-        'Created automated data analysis (RNQseq and proteomics) software using R',
-        'Conducted literature reviews for papers on neuroscience', 
-        'Developed visualizations for scRNAseq data + performed preprocessing and quality control'
+        'Created automated data analysis (RNAseq and proteomics) software using R',
+        'Conducted literature reviews for papers on neuroscience',
+        'Developed visualizations for scRNAseq data + performed preprocessing and quality control',
       ],
       skills: [
-        'R + RStudio + RShiny', 'Flask', 'HTML', 'Software Development Cycles (CI/CD)'
+        'R + RStudio + RShiny',
+        'Flask',
+        'HTML',
+        'Software Development Cycles (CI/CD)',
       ],
     },
   ];
@@ -68,7 +73,7 @@ function Experience() {
 
       <div className="relative border-l-4 border-pink-300 ml-4">
         {experiences.map((exp, index) => (
-          <div key={index} className="mb-10 ml-6">
+          <div key={index} className="mb-10 ml-6 relative">
             {/* Dot on the timeline */}
             <div className="absolute -left-3.5 w-7 h-7 bg-pink-400 rounded-full border-4 border-white z-10" />
 
@@ -78,23 +83,39 @@ function Experience() {
 
             {/* Projects */}
             <div className="mt-2">
-                <h4 className="font-semibold text-gray-800">Projects:</h4>
-                <ul className="list-disc list-inside text-base text-gray-800">
-                  {exp.projects.map((point, idx) => (
-                    <li key={idx}>{point}</li>
-                  ))}
-                </ul>
+              <h4 className="font-semibold text-gray-800">Projects:</h4>
+              <ul className="list-disc list-inside text-base text-gray-800">
+                {exp.projects.map((point, idx) => (
+                  <li key={idx}>{point}</li>
+                ))}
+              </ul>
             </div>
 
             {/* Skills */}
             <div className="mt-2">
-                <h4 className="font-semibold text-gray-800">Skills:</h4>
-                <ul className="list-disc list-inside text-base text-gray-800">
-                  {exp.skills.map((skill, idx) => (
-                    <li key={idx}>{skill}</li>
+              <h4 className="font-semibold text-gray-800">Skills:</h4>
+              <ul className="list-disc list-inside text-base text-gray-800">
+                {exp.skills.map((skill, idx) => (
+                  <li key={idx}>{skill}</li>
+                ))}
+              </ul>
+            </div>
+
+            {/* Publications (only render if exists) */}
+            {exp.publications && (
+              <div className="mt-2">
+                <h4 className="font-semibold text-gray-800">Publications:</h4>
+                <ul className="list-disc list-inside text-base text-blue-600">
+                  {exp.publications.map((pub, idx) => (
+                    <li key={idx}>
+                      <a href={pub} target="_blank" rel="noopener noreferrer">
+                        {pub}
+                      </a>
+                    </li>
                   ))}
                 </ul>
               </div>
+            )}
           </div>
         ))}
       </div>
